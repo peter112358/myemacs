392745c460ebbb4a08b3573ec9b1367d6b1654b1
wupingteng <pingteng.wu@weimob.com>
2018-10-05 13:19:37 +0800

    delete some key bind

--- init.el
+++ init.el
@@ -46,13 +46,8 @@
   (pallet-mode t))
 
 (use-package dumb-jump
-  :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
-         ("M-g i" . dumb-jump-go-prompt)
-         ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window))
   :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
-  :ensure)
+  :ensure t)
 
 (use-package doom-modeline
       :ensure t
